3b473083f2f8c9d4e8b2ced797bd273035c1fedd
Dipti104706 <diptimayeebehura104@gmail.com>
2021-11-16 21:33:44 +0530

    [Dipti]Add: FundoosModels folder added to store all model classes

---
+++ FundoosModels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundoosModels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
