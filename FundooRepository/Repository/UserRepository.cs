3316f66239bd5cee21e7d3f25766bdee6a260265
Dipti104706 <diptimayeebehura104@gmail.com>
2021-11-16 22:05:42 +0530

    [Dipti]Add: User Repository class added

---
+++ FundooRepository/Repository/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using FundooModels;
+using FundooRepository.Context;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FundooRepository.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserContext userContext;
+        public UserRepository(IConfiguration configuration, UserContext userContext)
+        {
+            this.Configuration = configuration;
+            this.userContext = userContext;
+        }
+
+        public IConfiguration Configuration { get; }
+        public string Register(RegisterModel userData)
+        {
+            try
+            {
+                //checking for email validation
+                var validEmail = this.userContext.Users.Where(x => x.Email == userData.Email).FirstOrDefault();
+                if (validEmail == null)
+                {
+                    if (userData != null)
+                    {
+                        // Add the data to the database
+                        this.userContext.Add(userData);
+                        // Save the change in database
+                        this.userContext.SaveChanges();
+                        return "Registration Successful";
+                    }
+                    return "Registration UnSuccessful";
+                }
+                return "Email Id Already Exists";
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
