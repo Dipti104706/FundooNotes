d228087b97bdc8bbe9a7df82ba332a4fc1d8da36
Dipti104706 <diptimayeebehura104@gmail.com>
2021-11-18 22:19:19 +0530

    [Dipti]Refactor: Regex Expressions created for register model properties

--- FundoosModels/RegisterModel.cs
+++ FundoosModels/RegisterModel.cs
@@ -7,15 +7,19 @@ namespace FundooModels
 {
     public class RegisterModel
     {
-        [Required]
+        [Key]
         public string UserId { get; set; }
         [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{2,}$", ErrorMessage = "Please enter a valid First Name")]
         public string FirstName { get; set; }
         [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{2,}$", ErrorMessage = "Please enter valid a Last Name")]
         public string LastName { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z0-9] +[._+-]{0, 1}[a-zA-Z0-9]*@[a-zA-Z0-9]{1,10}.[a-zA-Z]{ 2,10}[.]*[a-zA-Z]*$", ErrorMessage = "Please enter a valid Email")]
         public string Email { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z0-9]{1,}[A-Z]*[0-9]*[@&#%$*_-]+[a-zA-Z0-9]*$", ErrorMessage = "Please enter a valid Password")]
         public string Password { get; set; }
     }
 }
