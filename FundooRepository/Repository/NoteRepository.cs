e44905356841aacda655a9fedbd4c1235c7a5a8a
Dipti104706 <diptimayeebehura104@gmail.com>
2021-11-21 21:50:15 +0530

    [Dipti]Add: Notesrepository class added

---
+++ FundooRepository/Repository/NoteRepository.cs
@@ -0,0 +1,46 @@
+﻿using FundooModels;
+using FundooRepository.Context;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace FundooRepository.Repository
+{
+    public class NoteRepository
+    {
+        //Creating object for Usercontext
+        private readonly UserContext userContext;
+        
+        //Declaring parameterized constructor
+        public NoteRepository(IConfiguration configuration, UserContext userContext)
+        {
+            this.Configuration = configuration;
+            this.userContext = userContext;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        //Method for adding notes in the fundoo note application
+        public string Note(NoteModel noteModel)
+        {
+            try
+            {
+                if (noteModel != null)
+                {
+                    // Add the data to the database
+                    this.userContext.Notes.Add(noteModel);
+                    // Save the change in database
+                    this.userContext.SaveChanges();
+                    return "Note is Added";
+                }
+                else
+                {
+                    return "Adding note unsuccessful";
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
