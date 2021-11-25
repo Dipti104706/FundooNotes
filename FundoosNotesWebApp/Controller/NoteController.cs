22843050258f37e8551583861f2113835034e4a8
Dipti104706 <diptimayeebehura104@gmail.com>
2021-11-25 22:06:22 +0530

    [Dipti]Added:  Token authorization functionality for Note controller added

--- FundoosNotesWebApp/Controller/NoteController.cs
+++ FundoosNotesWebApp/Controller/NoteController.cs
@@ -1,5 +1,6 @@
 ﻿using FundooManager.Interface;
 using FundooModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace FundooNotesWebApp.Controller
 {
+    [Authorize]
     public class NoteController : ControllerBase
     {
         //Creating reference for Interface
--- FundoosNotesWebApp/Startup.cs
+++ FundoosNotesWebApp/Startup.cs
@@ -3,13 +3,16 @@ using FundooManager.Manager;
 using FundooRepository.Context;
 using FundooRepository.Interface;
 using FundooRepository.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace FundoosNotesWebApp
 {
@@ -37,7 +40,48 @@ namespace FundoosNotesWebApp
             //Adding swagger services
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1.0", new OpenApiInfo { Title = "FunDooNote", Version = "1.0" });
+                c.SwaggerDoc("v1.0", new OpenApiInfo { Title = "FunDooNotes", Description = "Keep Notes", Version = "1.0" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
+            });
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecretKey"]))  
+                };
             });
         }
 
@@ -60,6 +104,7 @@ namespace FundoosNotesWebApp
 
             app.UseRouting();//match request to endpoint
 
+            app.UseAuthorization();
             app.UseAuthorization();
 
             //it execute the matched endpoint
